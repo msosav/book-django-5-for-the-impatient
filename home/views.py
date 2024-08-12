abc7ac56dad2449c92db67f82247d1ca854e1263
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-08-12 13:00:10 -0500

    Implement Chapter 03

--- home/views.py
+++ home/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'Movies Store'
+    return render(request, 'home/index.html', {"template_data": template_data})
 
 
 def about(request):
-    return render(request, 'home/about.html')
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'home/about.html', {"template_data": template_data})
