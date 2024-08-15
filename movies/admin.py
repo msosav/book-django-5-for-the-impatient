bda1a698fc2d79cc29426aa7809614855f50ab1d
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-08-15 07:39:14 -0500

    Implement Chapter 09

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Movie
+from .models import Movie, Review
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ class MovieAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
